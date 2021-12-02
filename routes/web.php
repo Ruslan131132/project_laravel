34b4357bc26062aa4f8ca298f3792476e37b1795
Ruslan131132 <ruskhas131@mail.ru>
2021-12-03 00:15:11 +0300

    Добавлены новые маршруты, в том числе для префиксов админа и юзера

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,20 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('pages.index');
 })->name('index');
 
+Route::get('/logout', function(){
+    Auth::logout();
+    return redirect(route('index'));
+})->name('logout');
+
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('login');
+
+
 Route::name('user.')->group(function(){
     Route::view('/user',  'pages.user.user-main')->middleware('auth')->name('user-main');
+});
 
-    Route::get('/login', function(){
-//        if (Auth::check()){
-//            return redirect(route('user.user-main'));
-//        }
-        return redirect(route('index'));
-	})->name('login');
+Route::name('admin.')->group(function(){
+    Route::view('/admin-main',  'pages.admin.admin_main')->middleware('auth')->name('admin-main');
+    Route::view('/create-class',  'pages.admin.create_class')->middleware('auth')->name('create-class');
+    Route::view('/create-subject',  'pages.admin.create_subject')->middleware('auth')->name('create-subject');
+    Route::view('/create-employment',  'pages.admin.create_employment')->middleware('auth')->name('create-employment');
+    Route::view('/create-shedule',  'pages.admin.create-shedule')->middleware('auth')->name('create-shedule');
 
-//    Route::post('/login', []);
+    Route::post('/create-user', [\App\Http\Controllers\CreateUserController::class, 'save'])->name('create-user');;
 
 });
