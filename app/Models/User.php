532b924f3e1067c7a4cafccbbeda5e2fa4e82d33
Ruslan131132 <ruskhas131@mail.ru>
2021-12-09 00:59:38 +0300

    Добавлена связь OneToOne для Pupil-User и Teacher-User

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,26 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function teacher()
+    {
+        return $this->hasOne(Teacher::class);
+    }
+
+    public function pupil()
+    {
+        return $this->hasOne(Pupil::class, 'id', 'user_id');
+    }
     /**
      * The attributes that are mass assignable.
      *
