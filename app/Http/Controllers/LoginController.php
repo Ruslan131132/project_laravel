8afb475d32f71731c6bd22c6e011b637c24b188e
Ruslan131132 <ruskhas131@mail.ru>
2021-12-08 17:13:56 +0300

    Реализована логика аутентификации

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,8 +11,19 @@ class LoginController extends Controller
         $credentials = $request->only('user_id', 'password');
         if (Auth::attempt($credentials, $request->get('remember_token'))) {
             $user = auth()->user();
+
+            if ($user->isAdmin()){
+                return redirect(route('admin.admin-main'));
+            }
+            if ($user['user_type'] == 'Учитель'){
+                //Редирект на соответствующую страницу
+            }
+            if ($user['user_type'] == 'Ученик'){
+                //Редирект на соответствующую страницу
+            }
             return $user;
-//            return redirect()->intended('index');
+            //return redirect()->intended('index');
         }
+        return redirect(route('index'));
     }
 }
