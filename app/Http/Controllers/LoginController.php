480d34bf3dd24a580cc91e39b1fc437f1fc60560
Ruslan131132 <ruskhas131@mail.ru>
2021-12-12 18:46:16 +0300

    Закончена логика аутентификации

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,21 +11,10 @@ class LoginController extends Controller
     public function login(Request $request){
         $credentials = $request->only('user_id', 'password');
         if (Auth::attempt($credentials, $request->get('remember_token'))) {
-            $user = auth()->user();
-
-            if ($user->isAdmin()){
-                return redirect(route('admin.admin-main'));
+            if (auth()->user()->isAdmin()){
+                return redirect(route('admin.main'));
             }
-            if ($user['user_type'] == 'Учитель'){
-                //Редирект на соответствующую страницу
-            }
-            if ($user['user_type'] == 'Ученик'){
-//                dd($user->pupil['name']);
-//                dd(Pupil::all()->first());
-                //Редирект на соответствующую страницу
-            }
-            return $user;
-            //return redirect()->intended('index');
+            return redirect(route('user.main'));
         }
         return redirect(route('index'));
     }
