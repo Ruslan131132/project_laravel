5346eb91efdbf6ddc2c400dc759f5665c14b2047
Ruslan131132 <ruskhas131@mail.ru>
2021-12-18 02:14:25 +0300

    Добавлена основная маршрутизация

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,17 @@ Route::name('user.')->group(function(){
 Route::name('admin.')->group(function(){
     Route::view('/admin-main', 'pages.admin.main')->middleware('auth')->middleware('admin')->name('main');
     Route::get('/admin-users', [\App\Http\Controllers\Admin\UsersController::class, 'index'])->middleware('auth')->middleware('admin')->name('users');
-    Route::view('/admin-classes',  'pages.admin.classes')->middleware('auth')->middleware('admin')->name('classes');
-    Route::view('/admin-subjects',  'pages.admin.subjects')->middleware('auth')->middleware('admin')->name('subjects');
-    Route::view('/admin-employment',  'pages.admin.employment')->middleware('auth')->middleware('admin')->name('employment');
-    Route::view('/admin-schedule',  'pages.admin.schedule')->middleware('auth')->middleware('admin')->name('schedule');
+    Route::get('/admin-classes',  [\App\Http\Controllers\Admin\ClassesController::class, 'index'])->middleware('auth')->middleware('admin')->name('classes');
+    Route::get('/admin-subjects',  [\App\Http\Controllers\Admin\SubjectsController::class, 'index'])->middleware('auth')->middleware('admin')->name('subjects');
+    Route::get('/admin-employment',  [\App\Http\Controllers\Admin\EmploymentController::class, 'index'])->middleware('auth')->middleware('admin')->name('employment');
+    //Route::view('/admin-schedule-page',  'pages.admin.schedule')->middleware('auth')->middleware('admin')->name('schedule-page');
+
+    Route::get('/admin-schedule',  [\App\Http\Controllers\Admin\ScheduleController::class, 'index'])->middleware('auth')->middleware('admin')->name('schedule');
+    Route::post('/admin-schedule',  [\App\Http\Controllers\Admin\ScheduleController::class, 'selectSchedule'])->middleware('auth')->middleware('admin')->name('schedule');
 
     Route::post('/create-user', [\App\Http\Controllers\Admin\UsersController::class, 'save'])->name('create-user');
     Route::post('/delete-user', [\App\Http\Controllers\Admin\UsersController::class, 'delete'])->name('delete-user');
+    Route::post('/create-class', [\App\Http\Controllers\Admin\ClassesController::class, 'addClass'])->name('create-class');
+    Route::post('/create-course', [\App\Http\Controllers\Admin\ClassesController::class, 'addCourse'])->name('create-course');
+    Route::post('/get-class-schedule', [\App\Http\Controllers\Admin\ScheduleController::class, 'getSchedule'])->name('get-class-schedule');
 });
