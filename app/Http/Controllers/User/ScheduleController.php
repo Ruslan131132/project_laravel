0015b24284b72732eecf395572e28fc84d4112e5
Ruslan131132 <ruskhas131@mail.ru>
2021-12-18 02:18:07 +0300

    Добавлен контроллер для страницы расписания ученкив/преподавателей

--- app/Http/Controllers/User/ScheduleController.php
+++ app/Http/Controllers/User/ScheduleController.php
@@ -1,10 +1,34 @@
 <?php
 
 namespace App\Http\Controllers\User;
-
+use App\Http\Controllers\Controller;
+use App\Models\Day;
+use App\Models\Employment;
+use App\Models\Shedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index(){
+        if (Auth::user()->user_type == 'Ученик'){
+            $schedule = Shedule::where('class_id', Auth::user()->pupil->class_id)->orderByRaw('day_number ASC, lesson_number ASC')->get();
+        } else {
+            $schedule = Shedule::Where(function($query)
+            {
+                foreach (Auth::user()->teacher->employment as $info){
+                    $query->where([
+                        ['class_id', $info->class_id],
+                        ['subject_id', $info->subject_id],
+                    ]);
+                }
+            })->orderByRaw('day_number ASC, lesson_number ASC')->get();
+        }
+
+        $days = Day::all();
+        return view('pages.user.schedule', [
+            'schedule' => $schedule,
+            'days' => $days
+        ]);
+    }
 }
