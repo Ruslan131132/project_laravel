16270adcae6de3909d70364b946dccc156b68c93
Ruslan131132 <ruskhas131@mail.ru>
2022-01-19 23:36:42 +0300

    Добавлен метод для получения списка онлайн-пользователей

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 class MainController extends Controller
 {
+    public function index(){
+        $users = User::select("*")
+            ->whereNotNull('last_seen')
+            ->get();
+        $count = 0;
+        foreach ($users as $user)
+            if(Cache::has('user-is-online-' . $user->user_id))
+                $count++;
 
+        return view('pages.admin.main', [
+            'all_users' => count($users),
+            'users_online' => $count
+        ]);
+    }
 }
