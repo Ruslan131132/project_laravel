86de4b22ccbcbfb0afa02d7c69df0ae08c26d406
Ruslan131132 <ruskhas131@mail.ru>
2022-01-19 23:45:43 +0300

    Добавлено сообщение об ошибке при возвращении на главную страницу и прописан сценарий для обращения через api

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class Authenticate extends Middleware
 {
@@ -14,7 +15,13 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if($request->is('api/*'))
+        {
+            throw new HttpResponseException(response()->error(['failure_reason'=>'Необходим акутальный ключ доступа'], 'Unauthorized Request', 401));
+        }
+
+        if (!$request->expectsJson()) {
+            session()->flash('error', 'Для доступа необходимо войти в аккаунт!');
             return route('index');
         }
     }
