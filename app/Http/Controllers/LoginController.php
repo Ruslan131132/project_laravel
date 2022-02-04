48b51117c7fe9e127eb341c6ae929a6d2cf368f0
Ruslan131132 <ruskhas131@mail.ru>
2022-02-05 02:27:55 +0300

    Незначительные изменения в синтаксисе

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function login(Request $request){
         $credentials = $request->only('user_id', 'password');
-        if (Auth::attempt($credentials, $request->get('remember_token'))) {
+        if (Auth::attempt($credentials, $request->remember_token)) {
             if (auth()->user()->isAdmin()){
-                return redirect(route('admin.main'));
+                return redirect()->route('admin.main');
             }
-            return redirect(route('user.main'));
+            return redirect()->route('user.main');
         }
         return redirect()->back()->with('error_auth', 'Неверный логин или пароль!');
     }
