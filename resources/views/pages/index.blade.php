720f06f7b28e2c406ca452ff1b9ab7343f9ccf4e
Ruslan131132 <ruskhas131@mail.ru>
2022-02-11 18:24:07 +0300

    Переписана главная страница сайта - все адаптивно и прекрасно ;)

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,7 +15,7 @@
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
             <img src="/img/mospolytech-logo-white.png" alt="logo" width="20" height="20" class="round"> Дневник
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#999" class="bi bi-list" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
