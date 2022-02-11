36cf2242248dc437ce2b4ad1c1645ddb1dc05f48
Ruslan131132 <ruskhas131@mail.ru>
2022-02-11 17:55:45 +0300

    Контроллер расписания(страница Админа) переписан согласно модели USER; Так же добавлено удаление свзяи между таблицами учителей и расписания(метод delete)

--- app/Http/Controllers/Admin/ScheduleController.php
+++ app/Http/Controllers/Admin/ScheduleController.php
@@ -10,6 +10,7 @@ use App\Models\Lesson;
 use App\Models\Schedule;
 use App\Models\Subject;
 use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -32,7 +33,7 @@ class ScheduleController extends Controller
             'classes' => $classes,
             'schedule' => $schedule,
             'days' => $days,
-            'teachers' => Teacher::all(),
+            'teachers' => User::where('user_type', 'Учитель')->get(),
             'subjects' => Subject::all(),
             'cabinets' => Cabinet::all(),
             'lessons' => $lessons
@@ -57,8 +58,8 @@ class ScheduleController extends Controller
                 ->where('lesson_number', $request->lesson_number)
                 ->first()
             ){
-                Session::flash('error', 'В указанное время преподаватель '.$teacher->surname.' '.$teacher->name.' '.$teacher->patronymic.' занят');
-                return response()->json(array('message'=> 'В указанное время преподаватель '.$teacher->surname.' '.$teacher->name.' '.$teacher->patronymic.' занят'));
+                Session::flash('error', 'В указанное время преподаватель '.$teacher->user->surname.' '.$teacher->user->name.' '.$teacher->user->patronymic.' занят');
+                return response()->json(array('message'=> 'В указанное время преподаватель '.$teacher->user->surname.' '.$teacher->user->name.' '.$teacher->user->patronymic.' занят'));
             }
         }
 
@@ -83,7 +84,6 @@ class ScheduleController extends Controller
     public function edit(Request $request){
         $schedule = Schedule::where('id', $request->input('schedule_id'))->first();
 
-//        return $schedule->teacher->first();
         if (!$schedule){
             Session::flash('error', ("Ошибка - информация не найдена!"));
             return response()->json(array('message'=> 'Ошибка - информация не найдена!'), 404);
@@ -94,23 +94,25 @@ class ScheduleController extends Controller
             'day_number' => $schedule->day_number,
             'lesson_number' => $schedule->lesson_number,
             'cabinet_id' => $request->cabinet_id,
-            ['id', '!=', $schedule->id]
+            ['id', '<>', $schedule->id]
         ])->first()){
             Session::flash('error', 'В указанное время кабинет "' . $check_schedule->cabinet->name . '" занят классом ' . $check_schedule->class->name);
             return response()->json(array('message'=> 'В указанное время кабинет "' . $check_schedule->cabinet->name . '" занят классом ' . $check_schedule->class->name ));
         }
-
-        //проверяем - нет ли у выбранных преподавателей занятости в это время
-        foreach (Teacher::whereIn('id', $request->teachers)->get() as $teacher){
-            if ($teacher->schedule
-                ->where('day_number', $schedule->day_number)
-                ->where('lesson_number', $schedule->lesson_number)
-                ->first()
-            ){
-                Session::flash('error', 'В указанное время преподаватель '.$teacher->surname.' '.$teacher->name.' '.$teacher->patronymic.' занят');
-                return response()->json(array('message'=> 'В указанное время преподаватель '.$teacher->surname.' '.$teacher->name.' '.$teacher->patronymic.' занят'));
+        if($request->teachers){
+            //проверяем - нет ли у выбранных преподавателей занятости в это время
+            foreach (Teacher::whereIn('id', $request->teachers)->get() as $teacher){
+                if ($teacher->schedule
+                    ->where('day_number', $schedule->day_number)
+                    ->where('lesson_number', $schedule->lesson_number)
+                    ->first()
+                ){
+                    Session::flash('error', 'В указанное время преподаватель '.$teacher->user->surname.' '.$teacher->user->name.' '.$teacher->user->patronymic.' занят');
+                    return response()->json(array('message'=> 'В указанное время преподаватель '.$teacher->user->surname.' '.$teacher->user->name.' '.$teacher->user->patronymic.' занят'));
+                }
             }
         }
+
 
         //Обновляем
         $schedule->update([
@@ -125,11 +127,24 @@ class ScheduleController extends Controller
     }
 
     public function delete(Request $request){
-        Schedule::find($request->schedule_id)->forceDelete();
+
+        $schedule = Schedule::find($request->schedule_id);
+        if ($pivot = $schedule->teacher()){
+            $pivot->detach();
+        };
+        $schedule->forceDelete();
+
+        if($schedule->trashed()){
+            return response()->json([
+                'warning' => true,
+                'message'=> 'Ошибка удаления',
+            ]);
+        }
         Session::flash('success', ("Данные успешно удалены!"));
         return response()->json([
             'success' => true,
-            'message'=> 'Данные успешно удалены'
+            'message'=> 'Данные успешно удалены',
+            'is_tr' => $schedule->trashed()
         ]);
     }
 }
