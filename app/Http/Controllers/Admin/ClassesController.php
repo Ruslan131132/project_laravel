3c6dd9e434189bb4cad8f65fdce2a1354da668e2
Ruslan131132 <ruskhas131@mail.ru>
2022-02-11 18:01:02 +0300

    Контроллер переписан согласано модели USER

--- app/Http/Controllers/Admin/ClassesController.php
+++ app/Http/Controllers/Admin/ClassesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\ClassInfo;
 use App\Models\Course;
-use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
@@ -18,10 +18,10 @@ class ClassesController extends Controller
             Session::put('current_subpage', 'Classes');
         }
 
-        $teachers_without_class = Teacher::whereNotIn('id', function ($query) {
+        $teachers_without_class = User::where('user_type', 'Учитель')->whereNotIn('user_id', function ($query) {
             $query->selectRaw('teacher_id')->from('classes');
         })->orderBy('surname', 'asc')->get();
-        $teachers = Teacher::orderBy('surname', 'asc')->get();
+        $teachers = User::where('user_type', 'Учитель')->orderBy('surname', 'asc')->get();
         $classes = ClassInfo::orderBy('name', 'asc')->get();
         $courses = Course::all();
         return view('pages.admin.classes', [
