1523f2cabf5df24aeb174dfc07ce14f59a5b647b
Ruslan131132 <ruskhas131@mail.ru>
2022-02-11 17:58:41 +0300

    Убраны неиспользуемые классы и Контроллер занятости переписан согласно модели USER

--- app/Http/Controllers/Admin/EmploymentController.php
+++ app/Http/Controllers/Admin/EmploymentController.php
@@ -3,26 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\ClassInfo;
-use App\Models\Course;
 use App\Models\Day;
-use App\Models\Employment;
 use App\Models\Lesson;
-use App\Models\Schedule;
-use App\Models\Subject;
-use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class EmploymentController extends Controller
 {
     public function index(){
-        $teachers = Teacher::orderBy('surname', 'asc')->get();
+        $teachers = User::where('user_type', 'Учитель')->orderBy('surname', 'asc')->get();
         if (!Session::has('current_page') || Session::get('current_page') != 'employment'){
             Session::put('current_page', 'employment');
-            Session::put('current_subpage', $teachers->first()->id);
+            Session::put('current_subpage', $teachers->first()->user_id);
         }
-        $schedule = $teachers->find(Session::get('current_subpage'))->schedule->sortBy([
+        $schedule = $teachers->where('user_id', Session::get('current_subpage'))->first()->teacher->schedule->sortBy([
             'day_number', 'lesson_number'
         ]);
         $days = Day::all();
@@ -39,24 +34,10 @@ class EmploymentController extends Controller
     }
 
     public function create(Request $request){
-
-        $employment = Employment::where('teacher_id', $request->teacher_id)
-            ->where('subject_id', $request->subject_id)
-            ->where('class_id', $request->class_id)
-            ->first();
-        if ($employment){
-            return back()->with('error', 'Такая занятость уже существует!');
-        }
-        Employment::create([
-            'teacher_id' => $request->teacher_id,
-            'subject_id' => $request->subject_id,
-            'class_id' => $request->class_id,
-        ]);
         return back()->with('success', 'Занятость успешно добавлена!');
     }
 
     public function delete(Request $request){
-        Employment::where('id', $request->input('employment_id'))->first()->forceDelete();
         return back()->with('success', 'Занятость удалена!');
     }
 }
